2e43deece055100dad7981fcd8787de286735f3c
Adilson <estudosimformatica@gmail.com>
2025-10-19 09:27:20 -0300

    first commit

---
+++ src/main/java/br/com/easy/aalife/AalifeApplication.java
@@ -0,0 +1,13 @@
+package br.com.easy.aalife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AalifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AalifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/easy/aalife/AalifeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.easy.aalife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AalifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
